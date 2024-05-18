c99dbfd95a38d170de692d31249691551dcd4d2d
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2024-05-18 14:16:19 +0800

    Added some test things

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+#added something
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts', include('accounts.urls')),
